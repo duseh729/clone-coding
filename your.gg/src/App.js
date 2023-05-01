d954865178867087cf715e496cd1d2e38f6f6ecc
DoYeon Ahn <duseh729@naver.com>
2023-05-01 13:38:30 +0900

    feat: 로고 및 검색창 퍼블리싱

--- your.gg/src/App.js
+++ your.gg/src/App.js
@@ -1,34 +1,37 @@
-import logo from "./logo.svg";
 import { React, useState } from "react";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import "./App.css";
 import styles from "./css/public.css";
 import nav from "./css/nav.module.css";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import logo from "./css/logo.module.css";
+import userSearch from "./css/user-search.module.css";
+import { faSun, faMoon, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Logo />
+      <UserSearch />
     </div>
   );
 }
 
+// navbar 컴포넌트
 function Navbar() {
   let [colorMode, setColorMode] = useState(true);
   return (
     <nav>
-      <ul className={nav.leftNavElement}>
+      <ul className={nav["left-nav-element"]}>
         <li>듀오찾기</li>
         <li>협곡찾기</li>
         <li>멀티서치 AI</li>
       </ul>
-      <ul className={nav.rightNavElement}>
+      <ul className={nav["right-nav-element"]}>
         <li>
           <button>KR</button>
         </li>
-        <div className=""></div>
         <li
           onClick={() => {
             colorMode ? setColorMode(false) : setColorMode(true);
@@ -42,6 +45,38 @@ function Navbar() {
   );
 }
 
+// 로고 및 검색창 컴포넌트
+function Logo() {
+  return (
+    <div style={{ marginBottom: "30px" }}>
+      <span className={logo["logo"]}>YOUR.GG</span>
+    </div>
+  );
+}
+
+// 검색창 컴포넌트
+function UserSearch() {
+  return (
+    <div className={userSearch.search}>
+      <div className={userSearch.country}>
+        <span>KR</span>
+        <span
+          onClick={(e) => {
+            e.target.classList.toggle("rotate");
+          }}
+        >
+          🔻
+        </span>
+      </div>
+      <input type="text" />
+      <div className={userSearch.icon}>
+        <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />{" "}
+      </div>
+    </div>
+  );
+}
+
+// 공용 함수
 const colorModeHandler = (target, flag) => {
   if (flag) {
     target.classList.remove("dark-mode");
@@ -51,7 +86,5 @@ const colorModeHandler = (target, flag) => {
     target.classList.remove("white-mode");
   }
 };
-
-// 개발 족같네
 
 export default App;
